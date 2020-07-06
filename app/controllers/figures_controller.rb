44ef4058c8545fe6a8e0e1b08aedfc9cfd65cd90
jessegan <jesse13579@gmail.com>
2020-07-06 17:07:13 -0400

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,61 @@
 class FiguresController < ApplicationController
-  # add controller methods
+
+  get '/figures' do
+    @figures = Figure.all
+
+    erb :'figures/index'
+  end
+
+  get '/figures/new' do
+    @titles = Title.all
+    @landmarks = Landmark.all
+
+    erb :'figures/new'
+  end
+
+  post '/figures' do
+    figure = Figure.create(params[:figure])
+
+    if !params[:title][:name].empty?
+      figure.titles << Title.find_or_create_by(params[:title])
+    end
+
+    if !params[:landmark][:name].empty?
+      figure.landmarks << Landmark.find_or_create_by(params[:landmark])
+    end
+
+    figure.save
+
+    redirect "/figures/#{figure.id}"
+  end
+
+  get '/figures/:id' do
+    @figure = Figure.find(params[:id])
+
+    erb :'figures/show'
+  end
+
+  get '/figures/:id/edit' do
+    @figure = Figure.find(params[:id])
+    @titles = Title.all 
+    @landmarks = Landmark.all
+
+    erb :'figures/edit'
+  end
+
+  patch '/figures/:id' do
+    figure = Figure.find(params[:id])
+    figure.update(params[:figure])
+
+    if !params[:title][:name].empty?
+      figure.titles << Title.find_or_create_by(params[:title])
+    end
+
+    if !params[:landmark][:name].empty?
+      figure.landmarks << Landmark.find_or_create_by(params[:landmark])
+    end
+
+    redirect "/figures/#{figure.id}"
+  end
+
 end
