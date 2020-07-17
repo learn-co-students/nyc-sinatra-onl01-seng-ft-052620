6bd5bf669f943d92ca7dc67b93f34a67a2f32d5d
ejguzman3988 <eriberto176@gmail.com>
2020-07-17 03:37:34 -0400

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,47 @@
 class FiguresController < ApplicationController
   # add controller methods
+  get '/figures' do 
+    @figures = Figure.all
+    erb :'/figures/index'
+  end
+  
+  get '/figures/new' do
+    @titles = Title.all
+    @landmarks = Landmark.all
+
+    erb :'/figures/new'
+  end
+
+  get '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    erb :'/figures/show'
+  end
+
+  get '/figures/:id/edit' do
+    @figure = Figure.find(params[:id])
+    @titles = Title.all
+    @landmarks = Landmark.all
+    erb :'/figures/edit'
+  end
+
+  post '/figures' do 
+    figure = Figure.create(params[:figure])
+    puts params
+    
+    if !params[:title].empty?
+      figure.titles << Title.create(params[:title])
+    end
+
+    if !params[:landmark].empty?
+      figure.landmarks << Landmark.create(params[:landmark])
+    end
+    figure.save
+    redirect "/figures/#{figure.id}"
+  end
+
+  patch '/figures/:id' do
+    figure = Figure.find_by(params[:id])
+    figure.update(params[:figure])
+    erb :'/figure/show'
+  end
 end
--- app/models/figure.rb
+++ app/models/figure.rb
@@ -1,3 +1,6 @@
 class Figure < ActiveRecord::Base
-  # add relationships here
+  # add relationships her
+  has_many :landmarks
+  has_many :figure_titles
+  has_many :titles, through: :figure_titles
 end
